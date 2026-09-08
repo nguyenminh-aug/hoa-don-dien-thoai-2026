0f4165e0d7846e81ab3f917ba90a53ffeb54efad
nguyenminh-aug <140772050+nguyenminh-aug@users.noreply.github.com>
2026-09-09 06:22:42 +0700

    Fix logo paths on GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -19,8 +19,8 @@ export default defineConfig({
         display: 'standalone',
         start_url: base,
         icons: [
-          { src: '/app-logo-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/app-logo-512.png', sizes: '512x512', type: 'image/png' }
+          { src: 'app-logo-192.png', sizes: '192x192', type: 'image/png' },
+          { src: 'app-logo-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
