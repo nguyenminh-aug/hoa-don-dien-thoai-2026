ccea23ec932d4886b64cebf32add04f9b79fdcbf
nguyenminh-aug <140772050+nguyenminh-aug@users.noreply.github.com>
2026-09-09 06:12:09 +0700

    Use unique GitHub Pages repository path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
-const base = process.env.GITHUB_ACTIONS === 'true' ? '/hoa-don-mobile/' : '/'
+const base = process.env.GITHUB_ACTIONS === 'true' ? '/hoa-don-dien-thoai-2026/' : '/'
 
 export default defineConfig({
   base,
